99421a30c7ac91d83919db213961190cff7f8948
AnshuRoy25 <24bph025@nith.ac.in>
2025-06-19 17:10:14 +0530

    updated server.py

--- server.py
+++ server.py
@@ -214,4 +214,5 @@ def load_session_chats():
 
 # --------- Run the Flask App ---------
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
